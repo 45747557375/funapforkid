e4224c6bdda6a102fd0e619c9c26deb0f94ed888
45747557375 <103441765+45747557375@users.noreply.github.com>
2022-04-21 11:44:10 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,43 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen3_animalgifs");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen3_peppa");
+});
+onEvent("label3", "click", function( ) {
+  open("https://www.youtube.com/c/peppapig");
+});
+onEvent("button3", "click", function( ) {
+  playSound("yt5s.com---Samsung-Basic-bell-Ringtone-Download-(128-kbps).mp3", true);
+});
+onEvent("image12", "click", function( ) {
+  stopSound("yt5s.com---Samsung-Basic-bell-Ringtone-Download-(128-kbps).mp3");
+  playSound("assets/elsananna.mp3", true);
+});
+onEvent("image13", "click", function( ) {
+  stopSound("assets/elsananna.mp3");
+});
+onEvent("button7", "click", function( ) {
+  stopSound("assets/elsananna.mp3");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen3_callelsa");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen3_abc");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("screen2");
+});
